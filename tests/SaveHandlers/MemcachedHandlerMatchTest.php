85eb218813358cd7d1b70a1a6aaf213d84237814
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-18 13:13:40 -0500

    Create MemcachedHandlerMatchTest.php

---
+++ tests/SaveHandlers/MemcachedHandlerMatchTest.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of Aplus Framework Session Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Session\SaveHandlers;
+
+/**
+ * Class MemcachedHandlerMatchTest.
+ *
+ * @runTestsInSeparateProcesses
+ */
+final class MemcachedHandlerMatchTest extends MemcachedHandlerTest
+{
+    protected array $config = [
+        'match_ip' => true,
+        'match_ua' => true,
+    ];
+}
